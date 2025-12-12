bd7411b5be17d0d93ecdfdd0a2f5b03ed22d791c
annetkkayesu <kayesuanet12@gmail.com>
2025-12-12 11:53:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//   if (req.url === '/about') {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('About Page\n');
-//   } else {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello, World!\n');
-//   }
-// });
-
-// server.listen(3000, () => {
-//   console.log('Server running at http://localhost:3000/');
-// });
-
-
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Home route
-app.get('/', (req, res) => {
-  res.send('Hello, World');
+const server = http.createServer((req, res) => {
+  if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  }
 });
 
-// About route (modify this in feature branch)
-app.get('/about', (req, res) => {
-  res.send('About Page');
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
 
-app.listen(port, () => {
-  console.log(Server running on http://localhost:${port});
-});
+
